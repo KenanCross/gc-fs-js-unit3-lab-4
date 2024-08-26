1ae83592d08161d2d497a926f32255f52ea29dc9
kenancross <kenancross@gmail.com>
2024-08-26 19:16:09 -0400

    updated code to use template string and anonymous function

--- script.js
+++ script.js
@@ -1,23 +1,22 @@
-let name1 = 'Clark Kent';
-let name2 = 'Bruce Wayne';
-let name3 = 'Chris';
+let name1 = "Clark Kent";
+let name2 = "Bruce Wayne";
+let name3 = "Chris";
 
+let nameCompare = (n, n2, n3) => {
+	if (n.length > n2.length && n.length > n3.length) {
+		console.log(`${n} has the longest name.`);
+	} else if (n2.length > n.length && n2.length > n3.length) {
+		console.log(`${n2} has the longest name.`);
+	} else if (n3.length > n.length && n3.length > n2.length) {
+		console.log(`${n3} has the longest name.`);
+	} else if (n.length === n2.length && n.length > n3.length) {
+		console.log(`${n} and ${n2} tie for the longest name.`);
+	} else if (n2.length === n3.length && n2.length > n.length) {
+		console.log(`${n2} and ${n3} tie for the longest name.`);
+	} else if (n.length === n3.length && n.length > n2.length) {
+		console.log(`${n} and ${n3} tie for the longest name.`);
+	} else if (n.length === n2.length && n.length === n3.length) {
+		console.log(`All three names ${n}, ${n2} and ${n3}, are the same length.`);
+	}
+};
 nameCompare(name1, name2, name3);
-
-function nameCompare(n, n2, n3) {
-    if (n.length > n2.length && n.length > n3.length) {
-        console.log(n + ' has the longest name.')
-    } else if (n2.length > n.length && n2.length > n3.length) {
-        console.log(n2 + ' has the longest name.')
-    } else if (n3.length > n.length && n3.length > n2.length) {
-        console.log(n3 + ' has the longest name.')
-    } else if (n.length === n2.length && n.length > n3.length) {
-        console.log(n + ' and ' + n2 + ' tie for the longest name.');
-    } else if (n2.length === n3.length && n2.length > n.length) {
-        console.log(n2 + ' and ' + n3 + ' tie for the longest name.');
-    } else if (n.length === n3.length && n.length > n2.length) {
-        console.log(n + ' and ' + n3 + ' tie for the longest name.');
-    } else if (n.length === n2.length && n.length === n3.length) {
-        console.log('All three names ' + n + ', ' + n2 + ' and ' + n3 + ', are the same length.');
-    }
-}
